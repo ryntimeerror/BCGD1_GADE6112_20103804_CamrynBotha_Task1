b9cac8a6b2ab8c289c0834c6ab76398da67374a2
ryntimeerror <camrynbotha@gmail.com>
2020-09-09 08:09:24 +0200

    Test

---
+++ GADE6112_20103804_CamrynBotha_Task1/GADE6112_20103804_CamrynBotha_Task1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GADE6112_20103804_CamrynBotha_Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //test
+        }
+    }
+}
---
+++ GADE6112_20103804_CamrynBotha_Task1/GADE6112_20103804_CamrynBotha_Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GADE6112_20103804_CamrynBotha_Task1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
